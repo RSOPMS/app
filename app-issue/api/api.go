ba4e76779b705b4435a38fd4a6797b850090c84d
anzearhar <anze.arhar@gmail.com>
2024-12-11 12:33:47 +0100

    fix(app-issue): #33 update routing

--- app-issue/api/api.go
+++ app-issue/api/api.go
@@ -32,22 +32,22 @@ func (s *ApiServer) Run() error {
 
 func (s *ApiServer) registerHandlers(router *http.ServeMux) {
 	// Middleware
-	stackNone := api.CreateMiddlewareStack(api.LoggingMiddleware)
+	stackLog := api.CreateMiddlewareStack(api.LoggingMiddleware)
 
 	// Project
 	projectHandler := project.NewProjectHandler(s.Db)
-	router.Handle("GET /projects/{$}", stackNone(api.CreateHandler(projectHandler.GetProjectsPage)))
-	router.Handle("GET /projects/{projectId}/{$}", stackNone(api.CreateHandler(projectHandler.GetProjectPage)))
-	router.Handle("GET /api/projects/table/{$}", stackNone(api.CreateHandler(projectHandler.GetProjectsTable)))
-	router.Handle("GET /api/projects/{projectId}/issues/table/{$}", stackNone(api.CreateHandler(projectHandler.GetIssuesTable)))
+	router.Handle("GET /projects", stackLog(api.CreateHandler(projectHandler.GetProjectsPage)))
+	router.Handle("GET /projects/{projectId}", stackLog(api.CreateHandler(projectHandler.GetProjectPage)))
+	router.Handle("GET /api/projects/table", stackLog(api.CreateHandler(projectHandler.GetProjectsTable)))
+	router.Handle("GET /api/projects/{projectId}/issues/table", stackLog(api.CreateHandler(projectHandler.GetIssuesTable)))
 
 	// Issue
 	issueHandler := issue.NewIssueHandler(s.Db)
-	router.Handle("GET /issues/{issueId}/{$}", stackNone(api.CreateHandler(issueHandler.GetIssuePage)))
-	router.Handle("GET /api/issues/{issueId}/comments/table/{$}", stackNone(api.CreateHandler(issueHandler.GetCommentsTable)))
+	router.Handle("GET /issues/{issueId}", stackLog(api.CreateHandler(issueHandler.GetIssuePage)))
+	router.Handle("GET /api/issues/{issueId}/comments/table", stackLog(api.CreateHandler(issueHandler.GetCommentsTable)))
 
 	// Health
 	healthHandler := health.NewHealthHandler()
-	router.Handle("GET /health/live/{$}", stackNone(api.CreateHandler(healthHandler.GetHealthLive)))
-	router.Handle("GET /health/ready/{$}", stackNone(api.CreateHandler(healthHandler.GetHealthReady)))
+	router.Handle("GET /health/live", stackLog(api.CreateHandler(healthHandler.GetHealthLive)))
+	router.Handle("GET /health/ready", stackLog(api.CreateHandler(healthHandler.GetHealthReady)))
 }
